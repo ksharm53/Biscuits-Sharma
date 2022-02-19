ff7c8fbda45baaf9bb654b62813d1bf21a0ed4f7
vpatel58 <98491156+vpatel58@users.noreply.github.com>
2022-02-18 18:01:16 -0700

    all changes are done and dead code is removed

--- Biscuit/main/java/com/biscuit/commands/project/AddProject.java
+++ Biscuit/main/java/com/biscuit/commands/project/AddProject.java
@@ -26,7 +26,6 @@ public class AddProject implements Command {
 	public boolean execute() throws IOException {
 
 		StringBuilder description = new StringBuilder();
-		//StringBuilder github = new StringBuilder();
 		StringBuilder team_members = new StringBuilder();
 		String line,line2,line1;
 		boolean yes = false;
@@ -51,15 +50,6 @@ public class AddProject implements Command {
 		yes = (line1.toLowerCase().equals("y"));
 
 		
-		//System.out.println(ColorCodes.BLUE +"Do you want to add team members?"+ ColorCodes.RESET);
-		//System.out.println(ColorCodes.BLUE + "Press 'y' for yes and 'n' for no."+ColorCodes.RESET);
-		//Scanner scan = new Scanner (System.in);
-		//char member = scan.next().charAt(0);
-		//System.out.println(member);
-		//System.out.println("How many members are in the team? ");
-		//Scanner sc = new Scanner (System.in);
-		//int no_of_members = sc.nextInt();
-		//System.out.println(no_of_members);
 		if (yes) {
 			
 			reader.setPrompt(ColorCodes.GREEN + "\nType names: " + ColorCodes.YELLOW + "\n(\\q to end writing)\n" + ColorCodes.RESET);
@@ -80,16 +70,8 @@ public class AddProject implements Command {
 		
 		project.github= reader.readLine();
 		
-		//while ((line1 = reader.readLine()) != null) {
-		//	if (line1.equals("\\q")) {
-		//		break;
-		//	}
-		//	github.append(line1).append("\n");
-		//	reader.setPrompt("");
-		//}
 
 		project.description = description.toString();
-		//project.github= github.toString();
 		project.team_members = team_members.toString();
 
 		reader.setPrompt(prompt);
