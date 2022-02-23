e05d28849df0abff4f5189ace3fd28a28eb33935
f2.1222257920 <ksharm53@sundevils.asu.edu>
2022-02-23 00:15:34 -0700

    Slack Buiscuit integration. Changes done for slack notification while Removing project, Adding release, Add Task to USer Story, Change Status Task, Add User Story to backlog, Edit User Story. There are multiple refactoring done as well.

---
+++ Biscuit/main/java/com/biscuit/utility/Constants.java
@@ -0,0 +1,18 @@
+package com.biscuit.utility;
+
+public final class Constants {
+
+	public static final String TAIGA_REGISTER_API_URL = "https://api.taiga.io/api/v1/auth/register";
+	public static final String AUTHORIZATION ="Authorization";
+	public static final String BEARER ="Bearer ";
+	public static final String ACCESS_TOKEN ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ1NjI5NTk5LCJqdGkiOiJhYmJiMTBkZWEzNzM0MDU3OTU2MThjNDYxODE4ZDEzMyIsInVzZXJfaWQiOjUwMTU3M30.oAeaDwMgltsptXguSyX29WXbldOwlxGXIzNrG4FBAIM";
+
+	public static final String RELEASE_ADD_MESSAGE = " : Release Added by Buiscuit";
+	public static final String RELEASE_EDIT_MESSAGE = " : Release Edited by Buiscuit";
+	public static final String TASK_ADD_MESSAGE = "Task created for: ";
+	public static final String ADD_USERSTORY_MESSAGE = "UserStory added to backlog by Buiscuit: ";
+	public static final String EDIT_USERSTORY_MESSAGE = "UserStory edited by Buiscuit: ";
+	public static final String EDIT_USERSTORY_STATUS_MESSAGE = "State of user story ";
+
+	
+}
