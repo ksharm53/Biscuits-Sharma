11cea5c7c2be2eee8d8b6d15d97e1ddf64a4fee8
Manish <manish.m.r.96@gmail.com>
2022-02-24 21:54:37 -0700

    CSS addition to the front end page.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,28 +11,28 @@ import CreateNewProject from '../pages/CreateNewProject';
 function App() {
   return (
     <div className="App">
-      
-      <h1>Project</h1>
+
+      <h1>Project - Biscuits</h1>
 
       <Router>
-            <div>
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/create-new-project">Create New Project</Link>
-                  </li>
-                </ul>
-              </nav>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/create-new-project">Create New Project</Link>
+              </li>
+            </ul>
+          </nav>
 
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/create-new-project" element={<CreateNewProject />} />
-              </Routes>
-            </div>
-          </Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-new-project" element={<CreateNewProject />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
