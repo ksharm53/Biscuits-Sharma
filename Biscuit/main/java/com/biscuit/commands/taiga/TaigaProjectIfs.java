8644f152a2b58922c31f11989b8a0fbefbab9cd5
f2.1222257920 <ksharm53@sundevils.asu.edu>
2022-03-01 09:59:11 -0700

    Implementation for get Project by SLug ID. Test Driven development

--- Biscuit/main/java/com/biscuit/commands/taiga/TaigaProjectIfs.java
+++ Biscuit/main/java/com/biscuit/commands/taiga/TaigaProjectIfs.java
@@ -1,9 +1,13 @@
 package com.biscuit.commands.taiga;
 
+import org.springframework.http.ResponseEntity;
+import com.biscuit.models.ProjectResponse;
 import io.fabric8.taiga.ProjectDTO;
 
 public interface TaigaProjectIfs {
 
 	ProjectDTO addTaigaProject(ProjectDTO projectResponse);
+
+	ResponseEntity<ProjectResponse> getbyTaigaProjectSlug(String slug);
 
 }
--- Biscuit/main/java/com/biscuit/utility/Constants.java
+++ Biscuit/main/java/com/biscuit/utility/Constants.java
@@ -5,13 +5,14 @@ public final class Constants {
 	public static final String ADD_TAIGA_API_URL = "https://api.taiga.io/api/v1/projects";
 	public static final String AUTHORIZATION ="Authorization";
 	public static final String BEARER ="Bearer ";
-	public static final String ACCESS_TOKEN ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ1OTMxNzc3LCJqdGkiOiI4Zjg5ZDYxODJlY2Q0NmFjYWQ1ZGY5NzM1ZTExMzZlYyIsInVzZXJfaWQiOjUwMTU3M30.bGeLCBMWlYDCQA95_IwrgExrHF8kHVbITBOujI52S18";
+	public static final String ACCESS_TOKEN ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ2MTEyNzY3LCJqdGkiOiI0OWFjYzE0OGE4ODI0MTkzOWMxOTQyOWUwNWNmZjM3NyIsInVzZXJfaWQiOjUwMTU3M30.TcfCdL2PnAXckXY8sh7Tm9dUaTzhobg-K3YsjrkzOSw";
 	public static final String RELEASE_ADD_MESSAGE = " : Release Added by Buiscuit";
 	public static final String RELEASE_EDIT_MESSAGE = " : Release Edited by Buiscuit";
 	public static final String TASK_ADD_MESSAGE = "Task created for: ";
 	public static final String ADD_USERSTORY_MESSAGE = "UserStory added to backlog by Buiscuit: ";
 	public static final String EDIT_USERSTORY_MESSAGE = "UserStory edited by Buiscuit: ";
 	public static final String EDIT_USERSTORY_STATUS_MESSAGE = "State of user story ";
+	public static final String GET_TAIGA_SLUG_NAME_API_URL = "https://api.taiga.io/api/v1/projects/by_slug?slug={slug}";
 
 	
 }
