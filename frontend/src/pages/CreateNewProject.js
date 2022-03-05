d2b661ade0e6688d5502a47dc451a5c2235a3e89
Manish <manish.m.r.96@gmail.com>
2022-03-04 19:50:58 -0700

    front end page new features addition

--- frontend/src/pages/CreateNewProject.js
+++ frontend/src/pages/CreateNewProject.js
@@ -2,7 +2,14 @@ import { useState } from "react";
 
 function CreateNewProject() {
   const [name, setName] = useState("");
+  const [connectSlackChannel, setConnectSlackChannel] = useState();
+  const [taiga, setTaiga] = useState();
+  const [teamMembers, setTeamMembers] = useState();
+  const [teamMembersString, setTeamMembersString] = useState();
   const [description, setDescription] = useState("");
+  const [githubUrl, setGithubUrl] = useState("");
+  const [slackChannel, setSlackChannel] = useState("");
+  const [slackToken, setSlackToken] = useState("");
 
   function onNameChange(event) {
     setName(event.target.value);
@@ -12,36 +19,134 @@ function CreateNewProject() {
     setDescription(event.target.value);
   }
 
+  function onSlackConnectChange(e) {
+    setConnectSlackChannel(e.target.value);
+  }
+
+  function onSetTaigaChange(e) {
+    setTaiga(e.target.value);
+  }
+
+  function onSetTeamMembersChange(e) {
+    setTeamMembers(e.target.value);
+  }
+
+  function onFormSubmit() {
+    // call api
+
+    const data = {
+      name,
+      description,
+      github: githubUrl,
+      team_members: teamMembersString,
+    };
+  }
+
   return (
     <div>
-      <form style={{ padding: "10px 20px" }}>
-        <input placeholder="Enter project name" value={name} onChange={onNameChange} />
+      <form style={{ padding: "10px 20px" }} onSubmit={onFormSubmit}>
+        <input
+          placeholder="Enter project name"
+          value={name}
+          onChange={onNameChange}
+        />
         <br />
-        <input placeholder="Enter project description" value={description} onChange={onDescriptionChange} />
+        <input
+          placeholder="Enter project description"
+          value={description}
+          onChange={onDescriptionChange}
+        />
         <br />
-
         <p>Do you want team members?</p>
-        <input type="radio" value="yes" /> Yes<br />
-        <input type="radio" value="no" /> No
+        <input
+          type="radio"
+          name="team"
+          value="true"
+          checked={teamMembers === "true"}
+          onChange={onSetTeamMembersChange}
+        />{" "}
+        Yes
         <br />
-        <textarea />
-
+        <input
+          name="team"
+          type="radio"
+          value="false"
+          checked={teamMembers === "false"}
+          onChange={onSetTeamMembersChange}
+        />{" "}
+        No
+        {teamMembers === "true" && (
+          <div>
+            <p>Add team members (comma seperated)</p>
+            <textarea
+              value={teamMembersString}
+              onChange={(e) => setTeamMembersString(e.target.value)}
+            />
+          </div>
+        )}
         <br />
         <p>Github URL</p>
-        <input type="text" />
-
+        <input
+          type="text"
+          value={githubUrl}
+          onChange={(e) => setGithubUrl(e.target.value)}
+        />
         <br />
-
         <p>Do you want to connect with Slack channel?</p>
-
-        <input type="radio" value="yes" /> Yes<br />
-        <input type="radio" value="no" /> No
+        <input
+          type="radio"
+          value="true"
+          name="slack-channel"
+          checked={connectSlackChannel === "true"}
+          onChange={onSlackConnectChange}
+        />{" "}
+        Yes
         <br />
+        <input
+          type="radio"
+          value="false"
+          name="slack-channel"
+          checked={connectSlackChannel === "false  "}
+          onChange={onSlackConnectChange}
+        />{" "}
+        No
+        {connectSlackChannel && (
+          <div>
+            <p>Slack channel name (add '#')</p>
+            <input
+              type="text"
+              value={slackChannel}
+              onChange={(e) => setSlackChannel(e.target.value)}
+            />
 
+            <p>Slack token</p>
+            <input
+              type="text"
+              value={slackToken}
+              onChange={(e) => setSlackToken(e.target.value)}
+            />
+          </div>
+        )}
+        <br />
         <p>Do you want to add this Project to Taiga?</p>
-
-        <input type="radio" value="yes" /> Yes<br />
-        <input type="radio" value="no" /> No
+        <input
+          type="radio"
+          value="true"
+          name="taiga"
+          checked={taiga === "true"}
+          onChange={onSetTaigaChange}
+        />{" "}
+        Yes
+        <br />
+        <input
+          type="radio"
+          value="false"
+          name="taiga"
+          checked={taiga === "false"}
+          onChange={onSetTaigaChange}
+        />{" "}
+        No
+        <br />
         <br />
         <button>Save</button>
       </form>
